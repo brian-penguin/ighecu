cc8ec3c18c56377295ef4c49aa141c58b1da5346
Brian Tenggren <btenggren20@gmail.com>
2022-09-02 16:26:05 -0400

    Add some stuff for our async webserver!
    Why:
    * This adds a home and request page that will render some string
    defaults and read the params in
    
    This change addresses this by:
    * Add test
    * add nrepl
    * add in routing, ring, defaults, and some other fun stuff

--- src/ighecu/ighecu.clj
+++ src/ighecu/ighecu.clj
@@ -1,5 +1,32 @@
 (ns ighecu.ighecu
-  "FIXME: my new org.corfield.new/scratch project.")
+  "FIXME: my new org.corfield.new/scratch project."
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str])
+  (:gen-class))
+
+(defn home-page
+  [_req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello World"})
+
+(defn request-page
+  [req]
+     {:status  200
+      :headers {"Content-Type" "text/html"}
+      :body    (->>
+                (pp/pprint req)
+                (str "Request Object: " req))})
+
+(defroutes app-routes
+  (GET "/" [] home-page)
+  (GET "/request" [] request-page)
+  (route/not-found "Error, page not found")
+  )
 
 (defn exec
   "Invoke me with clojure -X ighecu.ighecu/exec"
@@ -9,4 +36,10 @@
 (defn -main
   "Invoke me with clojure -M -m ighecu.ighecu"
   [& args]
-  (println "-main with" args))
+  (println "-main with" args)
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
---
+++ test/ighecu/ighecu_test.clj
@@ -0,0 +1,6 @@
+(ns ighecu.ighecu-test
+  (:require [clojure.test :as t :refer [deftest is testing]]))
+
+(deftest ighecu-test
+  (testing "TODO: fix"
+    (is (= :foo :bar))))
